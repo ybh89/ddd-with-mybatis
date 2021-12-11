4b29dc98ac8b17cfbef52c1ebeb6cac99df310f3
ybh89 <akass191@gmail.com>
2021-12-11 11:17:34 +0900

    프로젝트 생성

---
+++ src/main/java/com/demo/dddwithmybatis/DddWithMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dddwithmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWithMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddWithMybatisApplication.class, args);
+	}
+
+}
